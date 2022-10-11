fe8af85a350e1911aec706d7987af7fd02715b48
ablirysa <ysabellea@nycstudents.net>
2022-10-11 09:25:11 -0400

    finished

--- src/LabRunner.java
+++ src/LabRunner.java
@@ -8,5 +8,24 @@ public class LabRunner {
         System.out.println(vroom);
         System.out.println(kachow);
         System.out.println(fwoosh);
+
+        BabyParrot baby = new BabyParrot("Toucan", 3, 4.25);
+        baby.setName("Lil' Beans");
+        String info = baby.parrotInfo();
+        System.out.println(info);
+
+        System.out.println("Cows say \"moo\"!");
+        System.out.println(" * *\n* * *\n * *");
+        System.out.println("Now I know my\n A\n  B\n   C’s");
+        System.out.println("An \"i\"\nfor\nan eye");
+        System.out.println("The string \"\"\nis an empty string.");
+        System.out.println("The escape sequence for \\ is \\\\");
+        System.out.println("The escape sequence for \" is \\\"");
+        System.out.println("/A\\V/A\\\n\\V/A\\V/");
+        System.out.println("Use \"\\n\"\nfor a new line.");
+        System.out.println("My favorite punctuation mark is \"\\\"");
+
+
+
     }
 }
