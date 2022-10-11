702f75f7ff09431887a2fcd6207027e87e025bc8
ablirysa <ysabellea@nycstudents.net>
2022-10-11 09:15:59 -0400

    initial commit

---
+++ src/LabRunner.java
@@ -0,0 +1,12 @@
+public class LabRunner {
+    public static void main(String[] args) {
+        String vroom = new String("Mater");
+        String s = "Tater";
+        String kachow = new String(s);
+        String fwoosh = "Tots";
+
+        System.out.println(vroom);
+        System.out.println(kachow);
+        System.out.println(fwoosh);
+    }
+}
